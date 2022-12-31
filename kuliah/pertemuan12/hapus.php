1e8b62ad08593c89159d21fa2565be79aaf7ae97
argimukti <argimukti46@gmail.com>
2022-12-31 13:11:05 +0700

    login dan daftar user

---
+++ kuliah/pertemuan12/hapus.php
@@ -0,0 +1,26 @@
+<?php 
+session_start();
+if (!isset($_SESSION['login'])){
+  header("Location: login.php");
+}
+require 'functions.php';
+// jika tidak ada id di ur
+if (!isset($_GET['id'])){
+  header("Location: index.php");
+  exit;
+}
+
+// ngambil id dari url
+$id = $_GET['id'];
+
+// functions hapus
+if (hapus($id) > 0){
+    echo "<script> 
+      alert('Data Berhasil di hapus'); document.location.href = 'index.php'; 
+    </script>";
+  } else {
+    echo "<script> alert ('Data Gagal hapus') </script>";
+  }
+
+
+?>
